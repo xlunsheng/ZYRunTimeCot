7f5a29a55a535840d91ca55e882250937e4350b2
谢伦生 <xielunsheng@xielunshengdeMac.local>
2018-09-07 16:16:44 +0800

    First 2 commit

---
+++ runtime/ZYRunTimeCot/ZYRunTimeCot/MyTest.h
@@ -0,0 +1,13 @@
+//
+//  MyTest.h
+//  ZYRunTimeCot
+//
+//  Created by 谢伦生 on 2018/9/7.
+//  Copyright © 2018年 谢伦生. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyTest : NSObject
+- (void) sayHi;
+@end
---
+++ runtime/ZYRunTimeCot/ZYRunTimeCot/MyTest.m
@@ -0,0 +1,16 @@
+//
+//  MyTest.m
+//  ZYRunTimeCot
+//
+//  Created by 谢伦生 on 2018/9/7.
+//  Copyright © 2018年 谢伦生. All rights reserved.
+//
+
+#import "MyTest.h"
+
+@implementation MyTest
+- (void) sayHi
+{
+    NSLog(@"Hit,This id MY Test");
+}
+@end
